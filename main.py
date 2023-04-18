535cea73c0ffcde6b1f1d586f364114ca8b4d702
Florent Poinsaut <florent@poinsaut.fr>
2023-04-18 17:54:16 +0200

    enhancements

--- main.py
+++ main.py
@@ -8,6 +8,7 @@
 # -*- coding: utf-8 -*-
 
 import os
+import sys
 
 import google_auth_oauthlib.flow
 import googleapiclient.discovery
@@ -18,6 +19,9 @@ scopes = ["https://www.googleapis.com/auth/youtube.force-ssl"]
 # Disable OAuthlib's HTTPS verification when running locally.
 # *DO NOT* leave this option enabled in production.
 os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
+
+# Enable dry-run mode
+dry_run = False
 
 api_service_name = "youtube"
 api_version = "v3"
@@ -35,7 +39,7 @@ class YouTubeHandler(object):
 
     def get_channel_info(self):
         request = self.yt.channels().list(
-          part="snippet,contentDetails,statistics",
+          part="contentDetails",
           mine=True
         )
         return request.execute()
@@ -50,6 +54,13 @@ class YouTubeHandler(object):
         )
         return request.execute()
 
+    def get_video(self, id):
+        request = self.yt.videos().list(
+          part="snippet",
+          id=id
+        )
+        return request.execute()['items'][0]
+
     def desc_find_replace(self, find_text, replace_text):
       response = self.get_playlist_videos()
 
@@ -57,36 +68,40 @@ class YouTubeHandler(object):
 
       while ("nextPageToken" in response):
         for item in response["items"]:
-          if find_text in item["snippet"]["description"]:
-            print("updating video: " + item["snippet"]["title"] + " https://www.youtube.com/watch?v=" + item["snippet"]["resourceId"]["videoId"])
-            self.replace_video_description(item["snippet"], find_text, replace_text)
+          description = "\n".join(item["snippet"]["description"].splitlines())
+          if find_text in description:
+            video = self.get_video(item["snippet"]["resourceId"]["videoId"])
+            print("updating video: " + video["snippet"]["title"] + " https://www.youtube.com/watch?v=" + video["id"])
+            self.replace_video_description(video, find_text, replace_text)
         response = self.get_playlist_videos(response["nextPageToken"])
 
       for item in response["items"]:
-          if find_text in item["snippet"]["description"]:
-            print("updating video: " + item["snippet"]["title"] + " https://www.youtube.com/watch?v=" + item["snippet"]["resourceId"]["videoId"])
-            self.replace_video_description(item["snippet"], find_text, replace_text)
+        description = "\n".join(item["snippet"]["description"].splitlines())
+        if find_text in description:
+          video = self.get_video(item["snippet"]["resourceId"]["videoId"])
+          print("updating video: " + video["snippet"]["title"] + " https://www.youtube.com/watch?v=" + video["id"])
+          self.replace_video_description(video, find_text, replace_text)
 
-    def replace_video_description(self, snippet, find_text, replace_text):
+    def replace_video_description(self, video, find_text, replace_text):
+      video["snippet"]["description"] = ("\n".join(video["snippet"]["description"].splitlines())).replace(find_text, replace_text)
+      body={
+        "id": video["id"],
+        "snippet": video["snippet"]
+      }
+      if dry_run:
+        print(body)
+      else:
         request = self.yt.videos().update(
           part="snippet",
-          body={
-            "id": snippet["resourceId"]["videoId"],
-            "snippet": {
-              "title": snippet["title"],
-              "categoryId": "24",
-              "defaultLanguage": "en",
-              "description": snippet["description"].replace(find_text, replace_text)
-            }
-          }
+          body=body
         )
         return request.execute()
 
 def menu():
-  print(new_line)
-  find_text = input("Find: ")
-  print("")
-  replace_text = input("Replace: ")
+  print(new_line + 'Enter your search phrase: (Press CTRL + D (Unix) or CTRL + Z (Windows) to validate)')
+  find_text = sys.stdin.read()
+  print(new_line + 'Enter your replacement phrase: (Press CTRL + D (Unix) or CTRL + Z (Windows) to validate)')
+  replace_text = sys.stdin.read()
   print(new_line)
 
   yt = YouTubeHandler()
